974381f10615091c55421dd1b2020e86a7d38764
Dominick Confessore <domconfessore@gmail.com>
2023-07-27 16:00:56 -0400

    fixed connection.js file to run properly

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,6 @@
 const { connect, connection } = require('mongoose');
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
 
 const connectionString =
     'mongodb://127.0.0.1:27017/socialNetworkDB';
